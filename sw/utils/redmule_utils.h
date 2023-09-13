b3987efb07fd8c994905dadbb1cfe3c208b312ca
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2023-09-13 17:33:09 +0200

    Fixed error detection function in sw check.

--- sw/utils/redmule_utils.h
+++ sw/utils/redmule_utils.h
@@ -47,8 +47,8 @@ int redmule16_compare_int(uint32_t *actual_z, uint32_t *golden_z, int len) {
     actual_LSHWord = (uint16_t)(actual_word & 0x0000FFFF);
     golden_LSHWord = (uint16_t)(golden_word & 0x0000FFFF);
 
-    diff = (actual_LSHWord > golden_LSHWord) ? (actual_LSHWord - golden_LSHWord) : 0;
-    diff = (actual_LSHWord < golden_LSHWord) ? (golden_LSHWord - actual_LSHWord) : 0;
+    diff = (actual_LSHWord > golden_LSHWord) ? (actual_LSHWord - golden_LSHWord) :
+           (actual_LSHWord < golden_LSHWord) ? (golden_LSHWord - actual_LSHWord) : 0;
 
     if (diff > ERR) {
       error = 1;
@@ -62,8 +62,8 @@ int redmule16_compare_int(uint32_t *actual_z, uint32_t *golden_z, int len) {
     actual_MSHWord = (uint16_t)((actual_word >> 16) & 0x0000FFFF);
     golden_MSHWord = (uint16_t)((golden_word >> 16) & 0x0000FFFF);
 
-    diff = (actual_MSHWord > golden_MSHWord) ? (actual_MSHWord - golden_MSHWord) : 0;
-    diff = (actual_MSHWord < golden_MSHWord) ? (golden_MSHWord - actual_MSHWord) : 0;
+    diff = (actual_MSHWord > golden_MSHWord) ? (actual_MSHWord - golden_MSHWord) :
+           (actual_MSHWord < golden_MSHWord) ? (golden_MSHWord - actual_MSHWord) : 0;
 
     if (diff > ERR) {
       error = 1;
@@ -119,8 +119,8 @@ int redmule8_compare_int(uint32_t *actual_z, uint32_t *golden_z, int len) {
     actual_Byte0 = (uint8_t)(actual_word & 0x000000FF);
     golden_Byte0 = (uint8_t)(golden_word & 0x000000FF);
 
-    diff = (actual_Byte0 > golden_Byte0) ? (actual_Byte0 - golden_Byte0) : 0;
-    diff = (actual_Byte0 < golden_Byte0) ? (golden_Byte0 - actual_Byte0) : 0;
+    diff = (actual_Byte0 > golden_Byte0) ? (actual_Byte0 - golden_Byte0) :
+           (actual_Byte0 < golden_Byte0) ? (golden_Byte0 - actual_Byte0) : 0;
 
     if (diff > ERR) {
       error = 1;
@@ -132,8 +132,8 @@ int redmule8_compare_int(uint32_t *actual_z, uint32_t *golden_z, int len) {
     actual_Byte1 = (uint8_t)( (actual_word >> 8 ) & 0x000000FF);
     golden_Byte1 = (uint8_t)( (golden_word >> 8 ) & 0x000000FF);
 
-    diff = (actual_Byte1 > golden_Byte1) ? (actual_Byte1 - golden_Byte1) : 0;
-    diff = (actual_Byte1 < golden_Byte1) ? (golden_Byte1 - actual_Byte1) : 0;
+    diff = (actual_Byte1 > golden_Byte1) ? (actual_Byte1 - golden_Byte1) :
+           (actual_Byte1 < golden_Byte1) ? (golden_Byte1 - actual_Byte1) : 0;
 
     if (diff > ERR) {
       error = 1;
@@ -145,8 +145,8 @@ int redmule8_compare_int(uint32_t *actual_z, uint32_t *golden_z, int len) {
     actual_Byte2 = (uint8_t)( (actual_word >> 16 ) & 0x000000FF);
     golden_Byte2 = (uint8_t)( (golden_word >> 16 ) & 0x000000FF);
 
-    diff = (actual_Byte2 > golden_Byte2) ? (actual_Byte2 - golden_Byte2) : 0;
-    diff = (actual_Byte2 < golden_Byte2) ? (golden_Byte2 - actual_Byte2) : 0;
+    diff = (actual_Byte2 > golden_Byte2) ? (actual_Byte2 - golden_Byte2) :
+           (actual_Byte2 < golden_Byte2) ? (golden_Byte2 - actual_Byte2) : 0;
 
     if (diff > ERR) {
       error = 1;
@@ -158,8 +158,8 @@ int redmule8_compare_int(uint32_t *actual_z, uint32_t *golden_z, int len) {
     actual_Byte3 = (uint8_t)( (actual_word >> 24 ) & 0x000000FF);
     golden_Byte3 = (uint8_t)( (golden_word >> 24 ) & 0x000000FF);
 
-    diff = (actual_Byte3 > golden_Byte3) ? (actual_Byte3 - golden_Byte3) : 0;
-    diff = (actual_Byte3 < golden_Byte3) ? (golden_Byte3 - actual_Byte3) : 0;
+    diff = (actual_Byte3 > golden_Byte3) ? (actual_Byte3 - golden_Byte3) :
+           (actual_Byte3 < golden_Byte3) ? (golden_Byte3 - actual_Byte3) : 0;
 
     if (diff > ERR) {
       error = 1;
