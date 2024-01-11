7fb158adc7a80cd99b3fd72b60e78a4a74aa1f5a
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-01-11 11:57:37 +0100

    Parametrize array width and properly propagate streamer bandwidth parameter to testbench.

--- rtl/redmule_pkg.sv
+++ rtl/redmule_pkg.sv
@@ -18,6 +18,7 @@
  * 
  * RedMulE Package
  */
+
 `include "hci/typedef.svh"
 `include "hci/assign.svh"
 `include "hwpe-ctrl/typedef.svh"
@@ -39,7 +40,7 @@ package redmule_pkg;
   parameter int unsigned            BITW         = fpnew_pkg::fp_width(FPFORMAT);
   parameter int unsigned            ARRAY_HEIGHT = 8;
   parameter int unsigned            PIPE_REGS    = 3;
-  parameter int unsigned            ARRAY_WIDTH  = 24; /* Superior limit is ARRAY_HEIGHT*PIPE_REGS */
+  parameter int unsigned            ARRAY_WIDTH  = ARRAY_HEIGHT*PIPE_REGS; // Superior limit, smaller values are allowed.
   parameter int unsigned            TOT_DEPTH    = DATAW/BITW;
   parameter int unsigned            DEPTH        = TOT_DEPTH/ARRAY_HEIGHT;
   parameter int unsigned            STRB         = DATA_W/8;
