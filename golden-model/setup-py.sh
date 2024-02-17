dbb1cc5e6e6809bf7c1e50e6a1b31d98203865c5
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-02-17 13:52:32 +0100

    Add CI - step 2.

--- golden-model/setup-py.sh
+++ golden-model/setup-py.sh
@@ -5,11 +5,11 @@
 # Yvan Tortorella <yvan.tortorella@unibo.it>
 #
 
-export PYTHON=python3
+export PYTHON=python3.6
 export PENV=$(pwd)/venv
 $PYTHON -m venv $PENV
 source $PENV/bin/activate
-pip install --upgrade pip
+pip3 install --upgrade pip
 pip3 install numpy
 pip3 install torch
 deactivate
