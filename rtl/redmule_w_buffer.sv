e609d94dcb21076df77f1096eea09e0ee6ecb457
Maurus Item <itemm@student.ethz.ch>
2024-07-12 12:17:40 +0200

    [HW] Added parametric redundancy to W/X/Y/Z Buffers. - Buffers now have a REP parameter which sets the number of replicas in the internal FSM. - Replicas of FSM get voted per cycle - Main data-path not replicated.

--- rtl/redmule_w_buffer.sv
+++ rtl/redmule_w_buffer.sv
@@ -19,6 +19,9 @@
  * RedMulE W Buffer
  */
 
+`include "redundancy_cells/voters.svh"
+`include "common_cells/registers.svh"
+
 module redmule_w_buffer
   import fpnew_pkg::*;
   import redmule_pkg::*;
@@ -26,69 +29,76 @@ module redmule_w_buffer
 parameter int unsigned  DW       = 288               ,
 parameter fp_format_e   FpFormat = FP16              ,
 parameter int unsigned  Height   = ARRAY_HEIGHT      , // Number of PEs per row
-localparam int unsigned BITW     = fp_width(FpFormat), // Number of bits for the given format                          
+parameter int unsigned  REP      = 1                 , // Number of Replicas of Internal FSM
+localparam int unsigned BITW     = fp_width(FpFormat), // Number of bits for the given format
 localparam int unsigned H        = Height            ,
 localparam int unsigned D        = DW/BITW
 )(
   input  logic                             clk_i     ,
   input  logic                             rst_ni    ,
-  input  logic                             clear_i   ,
-  input  w_buffer_ctrl_t                   ctrl_i    ,
-  output w_buffer_flgs_t                   flags_o   ,
+  input  logic                   [REP-1:0] clear_i   ,
+  input  w_buffer_ctrl_t         [REP-1:0] ctrl_i    ,
+  output w_buffer_flgs_t         [REP-1:0] flags_o   ,
   output logic           [H-1:0][BITW-1:0] w_buffer_o,
-  input logic                     [DW-1:0] w_buffer_i
+  input logic                     [DW-1:0] w_buffer_i,
+  output logic                             fault_o
 );
+
+
+  // Counter to track the number of shifts per row
+  logic [REP-1:0][$clog2(H):0]    w_row_b, w_row_v, w_row_d, w_row_q;
+
+  for (genvar r = 0; r < REP; r++) begin: gen_next_state
+    always_comb begin : row_load_counter
+        if (clear_i[r])
+          w_row_v[r] = '0;
+        else if (ctrl_i[r].load)
+          w_row_v[r] = (w_row_q[r] + 1) % H;
+        else
+          w_row_v[r] = w_row_q[r];
+    end
+  end
+
+  `VOTEXXF(REP, w_row_v, w_row_d, fault_o);
+
+  // Default State
+  for (genvar r = 0; r < REP; r++) begin: gen_default_state
+    assign w_row_b[r] = '0;
+  end
+
+  `FF(w_row_q, w_row_d, w_row_b);
+
+  // From here on out we use the signal of the first replica.
+  // If a fault happens on it then we can detect it since there is no more recursive dependency
 
   // Find out usable bounds
   logic [$clog2(H):0]            count_limit;
   logic [$clog2(D):0]            depth;
 
-  assign depth       = (ctrl_i.cols_lftovr == '0) ? D : ctrl_i.cols_lftovr;
-  assign count_limit = (ctrl_i.rows_lftovr == '0) ? H : ctrl_i.rows_lftovr;
-
-
-  // Counter to track the number of shifts per row
-  logic [$clog2(H):0]            w_row_q, w_row_d;
-
-  always_comb begin : row_load_counter
-      if (clear_i)
-        w_row_d = '0;
-      else if (ctrl_i.load)
-        w_row_d = (w_row_q + 1) % H;
-      else
-        w_row_d = w_row_q;
-  end
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin : row_load_counter_ff
-    if(~rst_ni) begin
-      w_row_q <= '0;
-    end else begin
-      w_row_q <= w_row_d;
-    end
-  end
-
+  assign depth       = (ctrl_i[0].cols_lftovr == '0) ? D : ctrl_i[0].cols_lftovr;
+  assign count_limit = (ctrl_i[0].rows_lftovr == '0) ? H : ctrl_i[0].rows_lftovr;
 
   // Main storage element
   logic [D-1:0][H-1:0][BITW-1:0] w_buffer_d, w_buffer_q;
 
   always_comb begin: w_trailer_comb
-    if (clear_i) begin
+    if (clear_i[0]) begin
       w_buffer_d = '0;
 
-    end else if (ctrl_i.shift | ctrl_i.load) begin // Load always means shift as well
+    end else if (ctrl_i[0].shift | ctrl_i[0].load) begin // Load always means shift as well
 
       // Shift elements in in d direction
       for (int d = 0; d < D - 1; d++) w_buffer_d[d] = w_buffer_q[d+1];
       w_buffer_d[D - 1] = '0;
 
       // If load is set Overwrite (!) elements in w_row_q
-      if (ctrl_i.load) begin
+      if (ctrl_i[0].load) begin
         for (int d = 0; d < D; d++) begin
           // Elements outside of usable bounds get set to 0
-          if (d < depth && w_row_q < count_limit) begin
-            w_buffer_d[d][w_row_q] = w_buffer_i[d*BITW+:BITW];
+          if (d < depth && w_row_q[0] < count_limit) begin
+            w_buffer_d[d][w_row_q[0]] = w_buffer_i[d*BITW+:BITW];
           end else begin
-            w_buffer_d[d][w_row_q] = '0;
+            w_buffer_d[d][w_row_q[0]] = '0;
           end
         end
       end
@@ -98,13 +108,7 @@ localparam int unsigned D        = DW/BITW
     end
   end
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin : w_trailer_ff
-    if(~rst_ni) begin
-      w_buffer_q <= '0;
-    end else begin
-      w_buffer_q <= w_buffer_d;
-    end
-  end
+  `FF(w_buffer_q, w_buffer_d, '0);
 
   // Output assignment
   assign w_buffer_o = w_buffer_q[0];
--- rtl/redmule_x_buffer.sv
+++ rtl/redmule_x_buffer.sv
@@ -19,6 +19,9 @@
  * RedMulE X Buffer
  */
 
+`include "redundancy_cells/voters.svh"
+`include "common_cells/registers.svh"
+
 module redmule_x_buffer
   import fpnew_pkg::*;
   import redmule_pkg::*;
@@ -27,6 +30,7 @@ parameter int unsigned           DW        = 288,
 parameter fpnew_pkg::fp_format_e FpFormat  = fpnew_pkg::FP16,
 parameter int unsigned           Height    = ARRAY_HEIGHT,  // Number of PEs per row
 parameter int unsigned           Width     = ARRAY_WIDTH,   // Number of parallel index
+parameter int unsigned           REP       = 1,             // Number of Replicas of Internal FSM
 localparam int unsigned          BITW      = fpnew_pkg::fp_width(FpFormat), // Number of bits for the given format                          
 localparam int unsigned          H         = Height,
 localparam int unsigned          W         = Width,
@@ -34,128 +38,163 @@ localparam int unsigned          D         = DW/(H*BITW),
 localparam int unsigned          HALF_D    = D/2,
 localparam int unsigned          TOT_DEPTH = H*D
 )(
-  input  logic                                               clk_i     ,
-  input  logic                                               rst_ni    ,
-  input  logic                                               clear_i   , 
-  input  x_buffer_ctrl_t                                     ctrl_i    ,
-  output x_buffer_flgs_t                                     flags_o   ,
+  input  logic                                               clk_i,
+  input  logic                                               rst_ni,
+  input  logic                                     [REP-1:0] clear_i, 
+  input  x_buffer_ctrl_t                           [REP-1:0] ctrl_i,
+  output x_buffer_flgs_t                           [REP-1:0] flags_o,
   output logic                      [W-1:0][H-1:0][BITW-1:0] x_buffer_o,
-  input logic                                       [DW-1:0] x_buffer_i
+  input logic                                       [DW-1:0] x_buffer_i,
+  output logic                                               fault_o
 );
 
-`include "voters.svh"
-`include "common_cells/registers.svh"
-
-
 // W Index Counnter
-logic [$clog2(W):0] w_index_d, w_index_q;
-logic [$clog2(W):0] w_limit;
+logic [REP-1:0][$clog2(W):0] w_index_b, w_index_v, w_index_d, w_index_q;
+logic [REP-1:0][$clog2(W):0] w_limit_v;
+logic w_fault;
 
-assign w_limit = (ctrl_i.rows_lftovr != '0) ? ctrl_i.rows_lftovr : W;
+for (genvar r = 0; r < REP; r++) begin: gen_w_counter_next_state
+  assign w_limit_v[r] = (ctrl_i[r].rows_lftovr != '0) ? ctrl_i[r].rows_lftovr : W;
 
-always_comb begin
-  flags_o.full = 1'b0;
+  always_comb begin
+    flags_o[r].full = 1'b0;
 
-  if (w_index_q == w_limit || w_index_q == W) begin
-    w_index_d = '0;
-    flags_o.full = 1'b1;
-  end else if (clear_i) begin
-    w_index_d = '0;
-  end else if (ctrl_i.load) begin
-    w_index_d = w_index_q + 1;
-  end else begin
-    w_index_d = w_index_q;
-  end
-end
-
-`FF(w_index_q, w_index_d, '0);
-
-
-// Depth Shift Counter with partial deactivation
-logic [$clog2(D):0] d_shift_d, d_shift_q;
-logic [$clog2(D):0] empty_count_d, empty_count_q;
-logic [$clog2(TOT_DEPTH):0] depth;
-logic rst_d_shift, rst_empty;
-
-always_comb begin
-  if (rst_d_shift || clear_i) begin
-    d_shift_d = '0;
-  end else if (ctrl_i.blck_shift) begin
-    d_shift_d = d_shift_q + 2;
-  end else if (ctrl_i.d_shift) begin
-    d_shift_d = d_shift_q + 1;
-  end else begin
-    d_shift_d = d_shift_q;
-  end
-end
-
-`FF(d_shift_q, d_shift_d, '0);
-
-always_comb begin
-  if (clear_i || rst_empty) begin
-    empty_count_d = D;
-  end else if (ctrl_i.cols_lftovr != '0) begin
-    empty_count_d = ctrl_i.slots;
-  end else begin
-    empty_count_d = empty_count_q;
-  end
-end
-
-`FF(empty_count_q, empty_count_d, '0);
-
-always_comb begin : empty_gen_and_shift_count_rst
-  flags_o.empty = 1'b0;
-  rst_d_shift = 1'b0;
-  rst_empty = 1'b0;
-
-  if (d_shift_q == empty_count_q) begin
-    flags_o.empty = 1'b1;
-    rst_d_shift = 1'b1;
-    if (empty_count_q != depth) begin
-      rst_empty = 1'b1;
+    if (w_index_q[r] == w_limit_v[r] || w_index_q[r] == W) begin
+      w_index_v[r] = '0;
+      flags_o[r].full = 1'b1;
+    end else if (clear_i[r]) begin
+      w_index_v[r] = '0;
+    end else if (ctrl_i[r].load) begin
+      w_index_v[r] = w_index_q[r] + 1;
+    end else begin
+      w_index_v[r] = w_index_q[r];
     end
   end
 end
 
-assign depth = (ctrl_i.cols_lftovr == '0) ? TOT_DEPTH : ctrl_i.cols_lftovr;
+`VOTEXXF(REP, w_index_v, w_index_d, w_fault);
 
+for (genvar r = 0; r < REP; r++) begin: gen_w_counter_default_state
+  assign w_index_b[r] = '0;
+end
 
-// H shift counter
-logic [$clog2(H)-1:0]       h_index_d, h_index_q;
+`FF(w_index_q, w_index_d, w_index_b);
 
-always_comb begin
-  if (clear_i) begin
-    h_index_d = '0;
-  end else if (ctrl_i.h_shift) begin
-    h_index_d = h_index_q + 1;
-  end else begin
-    h_index_d = h_index_q;
+// Depth Shift Counter with partial deactivation
+logic [REP-1:0][$clog2(D):0] d_shift_b, d_shift_v, d_shift_d, d_shift_q;
+logic [REP-1:0][$clog2(D):0] empty_count_b, empty_count_v, empty_count_d, empty_count_q;
+logic [REP-1:0][$clog2(TOT_DEPTH):0] depth_v;
+logic [REP-1:0]rst_d_shift_v, rst_empty_v;
+logic depth_fault, empty_fault;
+
+for (genvar r = 0; r < REP; r++) begin: gen_d_shift_next_state
+  always_comb begin
+    if (rst_d_shift_v[r] || clear_i[r]) begin
+      d_shift_v[r] = '0;
+    end else if (ctrl_i[r].blck_shift) begin
+      d_shift_v[r] = d_shift_q[r] + 2;
+    end else if (ctrl_i[r].d_shift) begin
+      d_shift_v[r] = d_shift_q[r] + 1;
+    end else begin
+      d_shift_v[r] = d_shift_q[r];
+    end
   end
 end
 
-`FF(h_index_q, h_index_d, '0);
+`VOTEXXF(REP, d_shift_v, d_shift_d, depth_fault);
 
+for (genvar r = 0; r < REP; r++) begin: gen_d_shift_default_state
+  assign d_shift_b[r] = '0;
+end
+
+`FF(d_shift_q, d_shift_d, d_shift_b);
+
+for (genvar r = 0; r < REP; r++) begin: gen_empty_next_state
+  always_comb begin
+    if (clear_i[r] || rst_empty_v[r]) begin
+      empty_count_v[r] = D;
+    end else if (ctrl_i[r].cols_lftovr != '0) begin
+      empty_count_v[r] = ctrl_i[r].slots;
+    end else begin
+      empty_count_v[r] = empty_count_q[r];
+    end
+  end
+end
+
+`VOTEXXF(REP, empty_count_v, empty_count_d, empty_fault);
+
+for (genvar r = 0; r < REP; r++) begin: gen_empty_default_state
+  assign empty_count_b[r] = '0;
+end
+
+`FF(empty_count_q, empty_count_d, empty_count_b);
+
+for (genvar r = 0; r < REP; r++) begin: gen_depth_shift_counter_reset
+  always_comb begin : empty_gen_and_shift_count_rst
+    flags_o[r].empty = 1'b0;
+    rst_d_shift_v[r] = 1'b0;
+    rst_empty_v[r] = 1'b0;
+
+    if (d_shift_q[r] == empty_count_q[r]) begin
+      flags_o[r].empty = 1'b1;
+      rst_d_shift_v[r] = 1'b1;
+      if (empty_count_q[r] != depth_v[r]) begin
+        rst_empty_v[r] = 1'b1;
+      end
+    end
+  end
+
+  assign depth_v[r] = (ctrl_i[r].cols_lftovr == '0) ? TOT_DEPTH : ctrl_i[r].cols_lftovr;
+end
+
+
+// H shift counter
+logic [REP-1:0][$clog2(H)-1:0] h_index_b, h_index_v, h_index_d, h_index_q;
+logic h_counter_fault;
+
+for (genvar r = 0; r < REP; r++) begin: gen_h_counter_next_state
+  always_comb begin
+    if (clear_i[r]) begin
+      h_index_v[r] = '0;
+    end else if (ctrl_i[r].h_shift) begin
+      h_index_v[r] = h_index_q[r] + 1;
+    end else begin
+      h_index_v[r] = h_index_q[r];
+    end
+  end
+end
+
+`VOTEXXF(REP, h_index_v, h_index_d, h_counter_fault);
+
+for (genvar r = 0; r < REP; r++) begin: gen_h_counter_default_state
+  assign h_index_b[r] = '0;
+end
+
+`FF(h_index_q, h_index_d, h_index_b);
+
+// From here on out we use the signal of the first replica.
+// If a fault happens on it then we can detect it since there is no more recursive dependency
 
 // Main storage element
 logic [D-1:0][W-1:0][H-1:0][BITW-1:0]     x_pad_d, x_pad_q;
 logic [(D/2)-1:0][W-1:0][H-1:0][BITW-1:0] x_buffer_d, x_buffer_q;
 
 always_comb begin
-  if (clear_i) begin
+  if (clear_i[0]) begin
       x_pad_d = '0;
       x_buffer_d = '0;
 
   end else begin
     x_pad_d = x_pad_q;
     x_buffer_d = x_buffer_q;
-    if (ctrl_i.load) begin
+    if (ctrl_i[0].load) begin
       for (int d = 0; d < D; d++) begin
         for (int h = 0; h < H; h++) begin
-          x_pad_d[d][w_index_q][h] = ((H * d + h) < depth) ? x_buffer_i[(H * d + h) * BITW +: BITW] : '0;
+          x_pad_d[d][w_index_q[0]][h] = ((H * d + h) < depth_v[0]) ? x_buffer_i[(H * d + h) * BITW +: BITW] : '0;
         end
       end
     end
-    if (ctrl_i.d_shift) begin
+    if (ctrl_i[0].d_shift) begin
       for (int w = 0; w < W; w++) begin
         for (int h = 0; h < H; h++) begin
           for (int d = 0; d < D; d++) begin
@@ -165,7 +204,7 @@ always_comb begin
         end
       end
     end
-    if (ctrl_i.blck_shift) begin
+    if (ctrl_i[0].blck_shift) begin
       for (int w = 0; w < W; w++) begin
         for (int h = 0; h < H; h++) begin
           for (int d = 0; d < D; d++) begin
@@ -177,10 +216,10 @@ always_comb begin
         end
       end
     end
-    if (ctrl_i.h_shift) begin
+    if (ctrl_i[0].h_shift) begin
       for (int w = 0; w < W; w++) begin
         for (int h = 0; h < H; h++) begin
-          x_buffer_d[0][w][h_index_q] = x_buffer_q[1][w][h_index_q];
+          x_buffer_d[0][w][h_index_q[0]] = x_buffer_q[1][w][h_index_q[0]];
         end
       end
     end
@@ -198,5 +237,7 @@ generate
     end
   end
 endgenerate
+
+assign fault_o = w_fault || depth_fault || empty_fault || h_counter_fault;
 
 endmodule : redmule_x_buffer
