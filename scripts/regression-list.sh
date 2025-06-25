986b851c9b62fa4a9419b3bb34dbdff0334dc605
Andrea Belano <andrea.belano@studio.unibo.it>
2025-06-25 14:26:47 +0200

    Remove impossible configurations from regression list

--- scripts/regression-list.sh
+++ scripts/regression-list.sh
@@ -20,27 +20,18 @@ else
   PARAMS=(
       96 96 96
       128 128 128
+      48 48 48
       12 16 16
       24 16 16
       48 32 32
-      30 32 17
-      24 32 1
-      31 32 16
-      17 32 16
-      31 32 31
-      17 32 3
-      5  32 17
-      5  32 3
-      36 31 32
-      12 31 16
-      23 31 31
-      24 17 32
+      30 32 18
+      24 32 2
+      18 32 16
+      6  32 4
+      36 32 32
+      12 30 16
+      24 18 32
       24 20 32
-      23 17 33
-      23 20 33
-      3  11 32
-      17 13 16
-      17 13 17
   )
 
   i=0
@@ -58,7 +49,7 @@ else
 
       make golden M=$M N=$N K=$K 1>/dev/null 2>&1
       make sw-clean sw-build 1>/dev/null 2>&1
-      timeout $BASE_TIMEOUT make hw-run target=$Target > $PWD/target/sim/$Target/transcript_${M}_${N}_${K} > artifacts/logs/test_${M}_${N}_${K}.log
+      timeout $BASE_TIMEOUT make hw-run target=$Target > $PWD/target/sim/$Target/transcript_${M}_${N}_${K}
       if grep -rn "\[TB\] - Success!" "$PWD/target/sim/$Target/transcript_${M}_${N}_${K}" 1>/dev/null 2>&1; then
           echo -e "${Green}OK  ${EndColor}: M=$M N=$N K=$K"
       else
