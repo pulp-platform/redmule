1fce877b4018fa6058159ca29dba154ae57834cb
Francesco Conti <f.conti@unibo.it>
2025-12-24 12:46:27 +0100

    Add draft SystemRDL register interface for RedMulE
    This renewed register interface mimics identical structure to the XIF
    that is currently the only supported one on the development branch of
    RedMulE.

---
+++ rtl/ctrl/gen_regif.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+peakrdl regblock  redmule_regif.rdl -o regif/ --cpuif obi-flat --default-reset arst_n --hwif-report --addr-width 32
+peakrdl html      redmule_regif.rdl -o regif/html/
+peakrdl c-header  redmule_regif.rdl -o regif/hwpe_ctrl_target.h
+# PeakRDL uses unpacked structs to avoid issues at compile time, which is commendable, but incompatible with FIFOing the output of the job! (use portable sed syntax that works on both Linux and macOS)
+sed 's/typedef[[:space:]]\+struct\b/typedef struct packed/g' regif/redmule_regif_pkg.sv > regif/redmule_regif_pkg.sv.tmp && mv regif/redmule_regif_pkg.sv.tmp regif/redmule_regif_pkg.sv
