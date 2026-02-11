fddf96ae862996a9c9bb6b6cb3c99e476ba6b1ca
Francesco Conti <f.conti@unibo.it>
2026-02-11 22:07:06 +0100

    fix register interface with packed structs

--- rtl/ctrl/gen_regif.sh
+++ rtl/ctrl/gen_regif.sh
@@ -3,4 +3,4 @@ peakrdl regblock  redmule_regif.rdl -o regif/ --cpuif obi-flat --default-reset a
 peakrdl html      redmule_regif.rdl -o regif/html/
 peakrdl c-header  redmule_regif.rdl -o regif/hwpe_ctrl_target.h
 # PeakRDL uses unpacked structs to avoid issues at compile time, which is commendable, but incompatible with FIFOing the output of the job! (use portable sed syntax that works on both Linux and macOS)
-sed 's/typedef[[:space:]]\+struct\b/typedef struct packed/g' regif/redmule_regif_pkg.sv > regif/redmule_regif_pkg.sv.tmp && mv regif/redmule_regif_pkg.sv.tmp regif/redmule_regif_pkg.sv
+sed -E 's/typedef[[:space:]]+struct([[:space:]])/typedef struct packed\1/g' regif/redmule_regif_pkg.sv > regif/redmule_regif_pkg.sv.tmp && mv regif/redmule_regif_pkg.sv.tmp regif/redmule_regif_pkg.sv
